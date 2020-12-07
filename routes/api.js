19d3bcd6fd8fa00c2e967d77e9a475729a06e347
Volodymyr Baisa <biosvova@gmail.com>
2020-12-07 16:56:16 -0500

    html - correct paths, api - add routes

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,18 @@
-module.exports = function (app) {};
+const workout = require("../models");
+const { db } = require("../models/workout");
+
+module.exports = function (app) {
+    //get workout
+    app.get("/api/workout", (req, res) => {
+        db.workout.find({}).then((workout) => {});
+    });
+
+    // add exercise
+    app.put("/api/workouts/:id", (req, res) => {});
+
+    //create workout
+    app.post("/api/workouts", ({ body }, res) => {});
+
+    // get workouts in range
+    app.get("/api/workouts/range", (req, res) => {});
+};
