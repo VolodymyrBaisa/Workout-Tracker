6e446a106d2b1f2a51dad9f2da6a850e859a1386
Volodymyr Baisa <biosvova@gmail.com>
2020-12-07 16:43:55 -0500

    set routes, fix path in html add function to api

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+require("./routes/api.js")(app);
+require("./routes/html.js")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
