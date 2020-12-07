6e359ac53052df8b020a99fb1e4b5c175b0a352d
Volodymyr Baisa <biosvova@gmail.com>
2020-12-06 22:56:23 -0500

    add config in index modules, add html routes, add .env to gitignore, add morgan logger and refference to html routes

--- models/index.js
+++ models/index.js
@@ -1,1 +1,2 @@
+require("dotenv").config();
 module.exports = { workout: require("./workout") };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use(express.static("public"));
 
@@ -20,6 +22,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
