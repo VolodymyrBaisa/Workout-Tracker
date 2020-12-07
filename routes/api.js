280982a38b4aebaa792c468b2ad615327e695ea0
Volodymyr Baisa <biosvova@gmail.com>
2020-12-07 17:26:04 -0500

    api remove import, html fix path, seed change Workout to workout

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,39 @@
-const workout = require("../models");
-const { db } = require("../models/workout");
+const db = require("../models");
 
 module.exports = function (app) {
     //get workout
     app.get("/api/workout", (req, res) => {
-        db.workout.find({}).then((workout) => {});
+        db.workout
+            .find({})
+            .then((workout) => {
+                workout.forEach((w) => {
+                    let total = 0;
+                    w.exercises.forEach((e) => {
+                        total += e.duration;
+                    });
+                    workout.totalDuration = total;
+                });
+                res.json(workout);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
     });
 
     // add exercise
     app.put("/api/workouts/:id", (req, res) => {});
 
     //create workout
-    app.post("/api/workouts", ({ body }, res) => {});
+    app.post("/api/workouts", ({ body }, res) => {
+        db.workout
+            .create(body)
+            .then((w) => {
+                res.json(w);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    });
 
     // get workouts in range
     app.get("/api/workouts/range", (req, res) => {});
